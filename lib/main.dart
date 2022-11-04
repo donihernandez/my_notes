6e791b7a749bf0ff0affc0ee025d9a7047692ff1
Adonai Domingez <donihernandez003@gmail.com>
2022-11-04 13:40:29 -0400

    Integrate Firebase. Add Register and Login Views.

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                final User? user = FirebaseAuth.instance.currentUser;
+                final emailVerified = user?.emailVerified ?? false;
+
+                if (emailVerified) {
+                  print("You're a verified user");
+                } else {
+                  print("You need to verify your email first");
+                }
+                return const Text('Done');
+              default:
+                return const Text('Loading...');
+            }
+          },
+        ));
+  }
+}
